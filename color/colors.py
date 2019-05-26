a28bfb8a326d058180711a1c8d25439ac381c528
SLOW BUT SURE <43800668+happyfunea@users.noreply.github.com>
2019-05-27 00:51:47 +0700

    Add files via upload

---
+++ color/colors.py
@@ -0,0 +1,11 @@
+
+
+class colorku:
+    def __init__(self):
+        self.green = "\033[1;32;40m"
+        self.normal = "\033[0;37;40m"
+        self.under = "\033[2;37;40m"
+        self.dark = "\033[1;30;40m"
+        self.magenta = "\033[1;35;40m"
+        self.yellow = "\033[1;33;40m"
+        self.red = "\033[1;31;40m"
